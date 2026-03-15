fe8e3f9c36c6de1df113b66182566856f75649aa
xaionaro@dx.center <Dmitrii Okunev>
2026-03-15 10:40:29 +0000

    feat: add APK source files for jniservice (foreground service + boot receiver)

---
+++ cmd/jniservice/apk/src/com/xaionaro/jniservice/JNIServiceActivity.java
@@ -0,0 +1,21 @@
+package com.xaionaro.jniservice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JNIServiceActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("jniservice is running.\n\nConnect with:\n  jnictl --addr <device-ip>:50051 --insecure jni get-version");
+        tv.setPadding(48, 48, 48, 48);
+        tv.setTextSize(16);
+        setContentView(tv);
+
+        Intent intent = new Intent(this, JNIServiceForeground.class);
+        startForegroundService(intent);
+    }
+}
